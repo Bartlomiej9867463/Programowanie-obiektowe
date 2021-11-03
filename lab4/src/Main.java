89dc9aab8ddad4f3898caf685cfd0a71004c351d
Bartlomiej9867463 <73538587+Bartlomiej9867463@users.noreply.github.com>
2021-11-03 04:17:36 +0100

    Add files via upload

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,53 +1,24 @@
-public class Main {
-        public static void main ( String [] args ) {
-            //System.out.println(countChar("abca a,ą",'a'));
-            System.out.println(countSubStr("abcab abb,ą","ab"));
-            System.out.println(middle("abb"));
-        }
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-        public static int countChar(String str, char c) {
-            int zliczaczwystapien = 0;
+public class Main
+{
 
-            for(char litera : str.toCharArray()) {
-                if (litera==c)
-                    zliczaczwystapien+=1;
-            }
-            return zliczaczwystapien;
-            }
-        public static int countSubStr(String str, String SubStr) {
-            int pozycja=0;
-            int dlugoscnapisu=SubStr.length();
-            int dlugoscstringa=str.length();
-            int napispozycja=0;
-            int zliczacznapisow = 0;
+    public static void main(String[] args) {
+        System.out.println(zad4(10,"0.02","1000.00"));
 
-            while(dlugoscstringa-pozycja>=dlugoscnapisu)
-            {
-                while (str.charAt(pozycja+napispozycja)==str.charAt(napispozycja))
-                {
-                    napispozycja+=1;
-                    if (napispozycja==(dlugoscnapisu-1)){
-                        zliczacznapisow+=1;
-                        break;
-                    }
-                }
-                //System.out.println((pozycja));
-                napispozycja=0;
-                pozycja+=1;
-            }
-
-            return zliczacznapisow;
     }
-    public static String middle(String str) {
-        int zliczaczwystapien = 0;
-        String zwrot="";
-        if (str.length()%2==0) {
-            zwrot = String.valueOf(str.charAt((str.length() / 2)-1))+String.valueOf(str.charAt((str.length() / 2)));
+    public static BigDecimal zad4(int n, String p, String k){
+        BigDecimal P = new BigDecimal(p);
+        BigDecimal K = new BigDecimal(k);
+        for (int i=1;i<=n;i++)
+        {
+            K =K.add(K.multiply(P));
         }
-        else
-            zwrot = String.valueOf(str.charAt((str.length() / 2)));
-        return zwrot;
+        K = K.setScale(2, RoundingMode.CEILING);
+    return K;
     }
-
 }
-
